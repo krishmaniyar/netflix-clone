5f04be55a5ecfb4afb47f49666d9a75ca725de82
krish <krishmaniyar27@gmail.com>
2025-02-17 18:52:27 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/netflix-clone/", // 👈 Use your repo name here!
+  base: "/netflix-clone/",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
 });
